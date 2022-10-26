3fc433a97ce4e73ddef6ebe36db97978be312c2d
NicolasFirpo <firpo.nico1@gmail.com>
2022-10-26 13:16:12 -0300

    committed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-//import logo from './logo.svg';
 import './App.css';
-//import MyComponent from './components/MyComponent';
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ItemListContainer from './components/ItemListContainer';
